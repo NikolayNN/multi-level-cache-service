16cef7fcf2c9a9d1a9a05ece4e0633bc2157ea2b
Nikolay <nikolay.horushko@gmail.com>
2025-07-09 12:17:49 +0300

    fix build scripts

--- build_dev.sh
+++ build_dev.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-echo "1"
 
-echo "2"
 LABEL=DEV
-echo "3"
 docker build -t cooll3r/multi-level-cache:$(cat VERSION)-$LABEL .
-echo "4"
+docker push cooll3r/multi-level-cache:$(cat VERSION)-$LABEL
+
--- build_release.sh
+++ build_release.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-set -euo pipefail
 LABEL=RELEASE
 docker build -t cooll3r/multi-level-cache:$(cat VERSION)-$LABEL .
+docker push cooll3r/multi-level-cache:$(cat VERSION)-$LABEL
